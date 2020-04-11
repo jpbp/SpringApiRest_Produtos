476a3f9c62126372b845a05b7ec4b0cca9acccaa
João Paulo Barbosa Pena <41149684+jpbp@users.noreply.github.com>
2020-04-11 11:42:36 -0300

    swagger ui e heroku

--- apirest/src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ apirest/src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.produtoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins="*")
 public class ProdutoResource {
 	
 	@Autowired
@@ -24,26 +30,31 @@ public class ProdutoResource {
 	
 	
 	@GetMapping("/produtos")
+	@ApiOperation(value="retorna uma lista de produtos")
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
 	
 	@GetMapping("/produtos/{id}")
+	@ApiOperation(value="retorna um produto pelo id")
 	public Produto listaProdutoUnico( @PathVariable(value="id")long id){
 		return produtoRepository.findById(id);
 	}
 	
 	@PostMapping("/produtos")
+	@ApiOperation(value="esse metodo salva o produto")
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
 	@DeleteMapping("/produtos")
+	@ApiOperation(value="esse metodo deleta o produto")
 	public void delatarProduto( @RequestBody Produto produto){
 		 	produtoRepository.delete(produto);
 	}
 	
 	@PutMapping("/produtos")
+	@ApiOperation(value="esse metodo atualiza o produto")
 	public Produto AlterarProduto( @RequestBody Produto produto){
 		 return produtoRepository.save(produto);
 	}
